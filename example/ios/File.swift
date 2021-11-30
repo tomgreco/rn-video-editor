0bd8897a9203489739d769e4fa5b204400f83d73
tomgreco <tom.greco@scanifly.com>
2021-11-30 15:04:45 -0600

    chore: initial commit

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  RnVideoEditorExample
+//
+
+import Foundation
---
+++ ios/RnVideoEditor.h
@@ -0,0 +1,5 @@
+#import <React/RCTBridgeModule.h>
+
+@interface RnVideoEditor : NSObject <RCTBridgeModule>
+
+@end
